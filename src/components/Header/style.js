69aa407c05654672090c2668e24e4ea8559559b6
ruandiegl <RuanDiegl33@gmail.com>
2024-02-03 17:50:51 -0300

    first commit

---
+++ src/components/Header/style.js
@@ -0,0 +1,5 @@
+export default function Header() {
+    return(
+        <h1>To-do List</h1>
+    )
+}
---
+++ src/components/assets/Global.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{  
+        margin: 0px;
+        padding: 0px;
+        font-family: sans-serif;
+    }
+    body{
+        background-color: #16161a;
+
+    }
+`
